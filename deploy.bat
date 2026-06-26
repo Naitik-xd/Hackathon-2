2d8fc66f991f5bd501864099a99f79761c0847bb
Naitik-xd <Naitik-xd@users.noreply.github.com>
2026-06-26 16:40:29 +0530

    Frontend Battle 3.0 - NeuralOps submission

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,5 @@
-"C:\Program Files\Git\cmd\git.exe" init
-"C:\Program Files\Git\cmd\git.exe" remote remove origin
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/Naitik-xd/Hackathon-2.git
-"C:\Program Files\Git\cmd\git.exe" add .
+"C:\Program Files\Git\cmd\git.exe" config user.email "Naitik-xd@users.noreply.github.com"
+"C:\Program Files\Git\cmd\git.exe" config user.name "Naitik-xd"
 "C:\Program Files\Git\cmd\git.exe" commit -m "feat: NeuralOps landing page - Frontend Battle 3.0 submission"
 "C:\Program Files\Git\cmd\git.exe" branch -M main
 "C:\Program Files\Git\cmd\git.exe" push -u origin main --force
